731424ba30a19a63390adc9a12b6342c908c48e4
saracampss <saracampxx@gmail.com>
2023-05-23 14:55:32 -0300

    Falsificação de calculateIdentifierCompletness
    Co-authored-by: Thiagof99 <Thiago3399@hotmail.com>

--- T1-Project/src/test/java/entities/IdentifierTest.java
+++ T1-Project/src/test/java/entities/IdentifierTest.java
@@ -1,10 +1,15 @@
 package entities;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class IdentifierTest {
+  Identifier identifier1 = new Identifier("123", "233"); 
+
   @Test
-    void calculateIdentifierCompleteness() {
+    public void calculateIdentifierCompletenessTest() {
+      int completness = identifier1.calculateIdentifierCompleteness();
+      Assertions.assertEquals(completness, 100);
     }
 
 }
