6c146cb18ae0d45e8fd0613e63ab73e6e94055fa
Amanda Nobre <amandapnobre@gmail.com>
2023-05-23 21:08:28 -0300

    Falsificação de calculateAuthorCompleteness
    Signed-off-by: AmandaNbr <amandapnobre@gmail.com>
    Co-authored-by: Thais-ra <rotinasoftware@gmail.com>

--- T1-Project/src/test/java/entities/AuthorTest.java
+++ T1-Project/src/test/java/entities/AuthorTest.java
@@ -7,9 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Assertions;
 
 class AuthorTest {
+    Identifier identifier1 = new Identifier("123", "123"); 
+    Author author1 = new Author(identifier1, "brazilian", "brazil", "saoPaulo", "saoPaulo"); 
 
     @Test
     void calculateAuthorCompleteness() {
+        int completness = author1.calculateAuthorCompleteness();
+        Assertions.assertEquals(completness, 100);
     }
-
 }
