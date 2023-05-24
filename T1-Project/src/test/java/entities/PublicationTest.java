0c1aded8fbd5f96b6323a4a49442619d50be487a
Irwin Schmitt <irwin.schmitt@quintoandar.com.br>
2023-05-24 09:28:13 -0300

    Falsificação de calculatePublicationCompleteness
    Co-authored-by: yukioz <yukio.link@gmail.com>

--- T1-Project/src/test/java/entities/PublicationTest.java
+++ T1-Project/src/test/java/entities/PublicationTest.java
@@ -6,11 +6,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PublicationTest {
 
-    @Test
-    void calculateCompleteness() {
-    }
+    Identifier identifier = new Identifier("12345678910", "");
+    Author author = new Author(identifier, "brazilian", "Brazil", "Brasilia", "DF");
+    Publication publication = new Publication("title", "2020", "portuguese", author);
 
     @Test
-    void calculateAtomicCompleteness() {
+    void calculateCompleteness() {
+        assertEquals(100, publication.calculatePublicationCompleteness());
     }
 }
